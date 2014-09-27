eb330d465737b6f772b9878200e1142bcfdf71b4
Michael Crabill <crabillm@purdue.edu>
2014-09-27 14:49:53 -0400

    added Utility

---
+++ src/Utility.java
@@ -0,0 +1,22 @@
+
+public class Utility {
+
+	public static boolean inCheck(int teamID) {
+	
+		return false;
+	}
+	
+	public static boolean inStalemate(int teamID) {
+		
+		
+		return false;
+	}
+	
+	public static boolean inCheckMate(int teamID) {
+		
+		
+		return false;
+	}
+	
+	
+}
